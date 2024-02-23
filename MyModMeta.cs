8ee4aefca47c03a4b689fc098192f06732a30470
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2024-02-22 22:45:03 -0300

    Made bigger fontsize in Options menu

--- MyModMeta.cs
+++ MyModMeta.cs
@@ -15,15 +15,16 @@ namespace SincUltimate
         {
             var title = WindowManager.SpawnLabel();
             title.text = "Ultimate Difficulty - by daRedLoCo";
-            title.fontSize = 24;
+            title.fontSize = 32;
             var description = WindowManager.SpawnLabel();
+            description.fontSize = 24;
             description.text = "Get the ultimate difficulty for your game of SINC!";
             var thnks = WindowManager.SpawnLabel();
-            thnks.fontSize = 8;
+            thnks.fontSize = 12;
             thnks.text = "Special thanks goes to CF as he gave me the idea to make the mod and he did all the playtesting because I'm bad...";
             WindowManager.AddElementToElement(title.gameObject, parent.gameObject, new Rect(15, 15, 192, 32), Rect.zero);
             WindowManager.AddElementToElement(description.gameObject, parent.gameObject, new Rect(15, 60, 192, 32), Rect.zero);
-            WindowManager.AddElementToElement(thnks.gameObject, parent.gameObject, new Rect(15, 100, 192, 96), Rect.zero);
+            WindowManager.AddElementToElement(thnks.gameObject, parent.gameObject, new Rect(15, 100, 260, 96), Rect.zero);
         }
     }
 }
